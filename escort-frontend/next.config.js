1ed6bbe565c3f219c3deb136da010d12781b31a7
kahiga254 <adamskahiga@gmail.com>
2026-02-09 11:59:13 +0300

    fix: remove .npmrc with package-lock=false, update next.js

--- escort-frontend/next.config.js
+++ escort-frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: 'standalone', // Enable standalone output for better performance and smaller image size
 }
 
 module.exports = nextConfig
